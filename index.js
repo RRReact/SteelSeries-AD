902af92c1e9f7aa3417ca97cd661b4eee553c0df
Robert <robi15222@gmail.com>
2019-12-21 19:20:11 +0100

    refactor from inline events

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const button = document.querySelectorAll(".click");
+const text = document.querySelectorAll(".click");
 const edge = document.querySelector(".edge");
 const slider = document.getElementById("slide");
 const right = document.querySelector(".slide1")
+const left = document.querySelector(".slide2")
 const cursor = document.querySelector(".cursor")
 const all = document.body
 
+right.addEventListener("mouseenter", () => cursorColorChange("white", "black"));
+left.addEventListener("mouseenter", () => cursorColorChange("black", "white"));
+edge.addEventListener("mousedown", () => drag(event));
 window.onmousemove = (e) => {
     let x = e.clientX;
     let y = e.clientY
@@ -12,12 +16,9 @@ window.onmousemove = (e) => {
     cursor.style.top = y + "px"
 }
 
-const addSlideEventListener = () => {
-    edge.addEventListener("mousedown", slide)
-}
 
-const removeSlideEventListener = () => {
-    button.forEach(item => { item.innerHTML = "Drag the edge and explore" });
+const removeDragEventListener = () => {
+    text.forEach(item => { item.innerHTML = "Drag the edge and explore" });
     window.onmousemove = (e) => {
         cursor.style.display = "inline"
         let x = e.clientX;
@@ -25,22 +26,21 @@ const removeSlideEventListener = () => {
         cursor.style.left = x + "px"
         cursor.style.top = y + "px"
     }
-    addSlideEventListener();
-
 }
-const slide = (event) => {
+const drag = (event) => {
     cursor.style.display = "none"
     event.preventDefault();
-    button.forEach(item => { item.innerHTML = "<span><</span>Slide<span>></span>"; });
+    text.forEach(item => { item.innerHTML = "<span><</span>Drag<span>></span>"; });
     window.onmousemove = (e) => {
         let x = e.clientX;
         slider.style.left = x + "px"
     }
-    all.addEventListener("mouseup", removeSlideEventListener, true)
+    all.addEventListener("mouseup", removeDragEventListener, true)
 }
 const cursorShow = () => { cursor.style.display = "inline" }
 const cursorHide = () => { cursor.style.display = "none" }
 const cursorColorChange = (color, borderColor) => {
-    cursor.style.background = color
+    cursor.style.backgroundColor = color
     cursor.style.border = `1px solid ${borderColor}`
 }
+all.addEventListener("mouseenter", cursorShow);
