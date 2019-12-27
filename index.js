475a5795d3823b14cafd966782337d5c5a99953b
Robert <robi15222@gmail.com>
2019-12-27 19:43:07 +0100

    added responsive sections

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ const left = document.querySelector(".slide2");
 const colors = document.querySelectorAll(".colors");
 const white = document.querySelectorAll(".white-click");
 const black = document.querySelectorAll(".black-click");
+const features = document.querySelector(".features")
+const specifications = document.querySelector(".specifications");
+const main = document.querySelector(".main")
 const all = document.body;
+
+
+let x = window.matchMedia("(max-width:1366px)");
+let featuresSection = null;
+let specificationsSection = null;
 
 window.onmousemove = (e) => {
     let x = getComputedStyle(right).left;
@@ -65,6 +73,35 @@ const blackOrWhite = (color) => {
         gsap.to(right, 1, { left: 0, ease: "power2.in" })
     }
 }
+const responsive = x => {
+    function insterAfter(afterNode, newNode) {
+        return afterNode.parentNode.insertBefore(newNode, main.afterNode);
+    }
+    if (x.matches === true) {
+        //create 2 empty sections 
+        featuresSection = document.createElement("section");
+        specificationsSection = document.createElement("section");
+        //clone nodes
+        const clonedFeatures = features.cloneNode(true);
+        const clonedSpecifications = specifications.cloneNode(true);
 
+        //add cloned nodes to created sections 
+        featuresSection.appendChild(clonedFeatures)
+        specificationsSection.appendChild(clonedSpecifications)
+        //insert to document
+        const newFeaturesSection = insterAfter(main, featuresSection);
+        const newSpecificationsSection = insterAfter(newFeaturesSection, specificationsSection)
+        //add classes
+        newFeaturesSection.classList.add("responsive-features");
+        newSpecificationsSection.classList.add("responsive-specifications");
+        /// set display:none to "features" and "specifications"
+        specifications.style.display = "none";
+        features.style.display = "none";
+        //set overflow
+        all.style.overflow = "visible";
+        all.style.overflowX = "hidden";
 
-
+    }
+}
+responsive(x);
+x.addListener(responsive);
